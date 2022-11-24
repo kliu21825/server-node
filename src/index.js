a3fb31e4c7055ed927352c092083ef7045fa3657
Kevin Liu <kliu@southtexascollege.edu>
2022-11-23 23:50:02 -0600

    fixing an issue

--- src/index.js
+++ src/index.js
@@ -5,37 +5,30 @@ import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
-import basicAuth from 'express-basic-auth'
 
 import logger from './helpers/logger'
 import router from './routes'
 import { notFound, errorHandler } from './helpers/errors'
 
-const port = parseInt(process.env.PORT, 10) || 3000
+const port = Number(process.env.PORT)
 
 const app = express()
-
-app.use(
-  basicAuth({
-    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
-  }),
-)
 
 app.use(morgan(process.env.MORGAN_LOG))
 app.use(
   cors({
-    origin: process.env.ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     exposedHeaders: ['x-total-count', 'x-total-pages'],
   }),
 )
 app.use(helmet())
 app.use(bodyParser.json())
 
-app.use(router)
+app.use('/', router)
 
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(port, () =>
-  logger.info(`Application started at http://localhost:${process.env.PORT}`),
-)
+app.listen(port, () => {
+  logger.info(`Server running on port ${port}`)
+})
